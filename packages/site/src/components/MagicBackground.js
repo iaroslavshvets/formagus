2a65cc9379119385790b040177465d55d72469f8
iaroslavs <iaroslavs@wix.com>
2018-11-05 13:05:31 +0200

    loader

--- packages/site/src/components/MagicBackground.js
+++ packages/site/src/components/MagicBackground.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import Magic from '../public/images/magic.svg';
-
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
+import {getRandomColor} from '../utils/getRandomColor';
 
 const commonCss = {
   position: 'absolute',
