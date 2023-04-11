6b08a8f450d67bcee61b9a7fe669164433330592
iaroslavshvets <iaroslavs@wix.com>
2023-04-11 23:10:10 +0300

    webpack => rollup

---
+++ packages/lib/rollup.config.mjs
@@ -0,0 +1,22 @@
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import packageJson from './package.json' assert { type: 'json' };
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      plugins: [terser()]
+    },
+    {
+      file: packageJson.module,
+      format: 'es',
+      sourcemap: true,
+      plugins: [terser()]
+    }
+  ],
+  plugins: [typescript()]
+};
