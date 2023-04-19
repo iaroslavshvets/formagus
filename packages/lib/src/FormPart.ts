9271f4428ce1410c9b2d172562df9799d4ef8b06
iaroslavshvets <iaroslavs@wix.com>
2023-04-19 23:51:25 +0300

    try to commit

--- packages/lib/src/FormPart.ts
+++ packages/lib/src/FormPart.ts
@@ -5,9 +5,7 @@ import type {FormProps} from './Form/Form.types';
 export const FormPart = observer((props: {children: FormProps['children']}) => {
   const controller = useFormController();
 
-  return props.children(controller.API)
-
-
+  return props.children(controller.API);
 });
 
 (FormPart as any).displayName = 'FormagusFormPart';
