fd77fae07d11218374aee0b49fd619cbd640251b
iaroslavs <iaroslav.shvets@gmail.com>
2023-04-21 16:05:26 +0300

    fix typingds

--- packages/lib/src/Field/Field.tsx
+++ packages/lib/src/Field/Field.tsx
@@ -4,28 +4,35 @@ import type {JSXElementConstructor} from 'react';
 import {useField} from './useField';
 import {FieldContextProvider} from './FieldContext';
 import type {FieldProps} from './Field.types';
+import {CreateFormController} from '../FormController';
 
-export const Field = observer(<T extends JSXElementConstructor<any>>(props: FieldProps<T>) => {
-  const {formagus} = useField(props);
+export const Field = observer(
+  <T extends JSXElementConstructor<any>>(
+    props: Omit<FieldProps<T>, 'controller'> & {
+      controller?: CreateFormController;
+    },
+  ) => {
+    const {formagus} = useField(props as FieldProps<T>);
 
-  if (!formagus) {
-    return null;
-  }
+    if (!formagus) {
+      return null;
+    }
 
-  if (props.adapter) {
-    const Adapter: any = props.adapter;
-    return (
-      <FieldContextProvider value={formagus}>
-        <Adapter formagus={formagus} {...props.adapterProps} />
-      </FieldContextProvider>
-    );
-  }
+    if (props.adapter) {
+      const Adapter: any = props.adapter;
+      return (
+        <FieldContextProvider value={formagus}>
+          <Adapter formagus={formagus} {...props.adapterProps} />
+        </FieldContextProvider>
+      );
+    }
 
-  if (props.render) {
-    return <FieldContextProvider value={formagus}>{props.render({formagus: formagus!})}</FieldContextProvider>;
-  }
+    if (props.render) {
+      return <FieldContextProvider value={formagus}>{props.render({formagus: formagus!})}</FieldContextProvider>;
+    }
 
-  return <FieldContextProvider value={formagus}>{props.children}</FieldContextProvider>;
-});
+    return <FieldContextProvider value={formagus}>{props.children}</FieldContextProvider>;
+  },
+);
 
 (Field as any).displayName = 'FormagusField';
