0664892b375e11bb30ae387cd0ea86ee3095bef1
iaroslavs <iaroslav.shvets@gmail.com>
2023-05-15 17:14:13 +0300

    add tests for events

---
+++ packages/lib/__tests__/specs/subscriptions.tsx
@@ -0,0 +1,70 @@
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+import {reaction} from 'mobx';
+import {TestForm} from '../components/TestForm';
+import {Input} from '../components/Input';
+import {createFormController, Field} from '../../src';
+import {createTestFormDriver} from '../components/createTestFormDriver';
+import {eventually} from '../helpers/eventually';
+
+describe('Subscriptions', () => {
+  afterEach(() => cleanup());
+
+  it('onValidate, onSubmit reactions', async () => {
+    const submitTimeLogger = jest.fn();
+    const validateTimeLogger = jest.fn();
+
+    const controller = createFormController({
+      onSubmit: jest.fn(),
+      onValidate: jest.fn(),
+    });
+
+    let submitTimeCounter = 0;
+    let validateTimeCounter = 0;
+
+    const onValidateEvent = reaction(
+      () => controller.API.meta.isValidating,
+      (isValidating) => {
+        if (isValidating) {
+          validateTimeCounter = Date.now();
+        } else {
+          validateTimeLogger(Date.now() - validateTimeCounter);
+        }
+      },
+    );
+
+    const onSubmitEvent = reaction(
+      () => controller.API.meta.isSubmitting,
+      (isSubmitting) => {
+        if (isSubmitting) {
+          submitTimeCounter = Date.now();
+        } else {
+          submitTimeLogger(Date.now() - submitTimeCounter);
+        }
+      },
+    );
+
+    const wrapper = render(
+      <TestForm controller={controller}>
+        <Field name={TestForm.FIELD_ONE_NAME}>
+          <Input />
+        </Field>
+      </TestForm>,
+    ).container;
+
+    const formDriver = createTestFormDriver({wrapper});
+
+    formDriver.when.submit();
+
+    await eventually(() => {
+      const submitTime = submitTimeLogger.mock.calls[0][0];
+      const validateTime = validateTimeLogger.mock.calls[0][0];
+
+      expect(submitTime).toBeGreaterThanOrEqual(1);
+      expect(submitTime).toBeGreaterThanOrEqual(validateTime);
+
+      onSubmitEvent();
+      onValidateEvent();
+    });
+  });
+});
