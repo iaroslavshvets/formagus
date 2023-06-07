70405d679e31edc64bd6c6d5ab498a3a1d38cd25
Iaroslav Shvets <iaroslav.shvets@gmail.com>
2023-06-07 23:05:44 +0300

    add tests for events (#109)

---
+++ packages/lib/__tests__/specs/subscriptions.tsx
@@ -0,0 +1,51 @@
+import {cleanup, render} from '@testing-library/react';
+import React from 'react';
+import {reaction} from 'mobx';
+import {TestForm} from '../components/TestForm';
+import {Input} from '../components/Input';
+import {createFormController, Field} from '../../src';
+import {createTestFormDriver} from '../components/createTestFormDriver';
+import {eventually} from '../helpers/eventually';
+
+describe('Subscriptions', () => {
+  afterEach(() => cleanup());
+
+  it('onSubmit reaction', async () => {
+    const controller = createFormController({
+      onSubmit: jest.fn(),
+      onValidate: jest.fn(),
+    });
+
+    let submitStartTime = 0;
+    let submitTimeTotal = 0;
+
+    const unsubscribe = reaction(
+      () => controller.API.meta.isSubmitting,
+      (isSubmitting) => {
+        if (isSubmitting) {
+          submitStartTime = Date.now();
+        } else {
+          submitTimeTotal = Date.now() - submitStartTime;
+        }
+      },
+    );
+
+    const wrapper = render(
+      <TestForm controller={controller}>
+        <Field name={TestForm.FIELD_ONE_NAME}>
+          <Input />
+        </Field>
+      </TestForm>,
+    ).container;
+
+    const formDriver = createTestFormDriver({wrapper});
+
+    formDriver.when.submit();
+
+    await eventually(() => {
+      expect(submitTimeTotal).toBeGreaterThanOrEqual(1);
+
+      unsubscribe();
+    });
+  });
+});
